d5ae146b40b8fbd6a1ac5bd849a8f9fae9b6a58d
Noah Shofer <njshofer@gmail.com>
2017-06-09 11:25:59 -0700

    updates to newNEM, created modular modNEM which uses the package testpack/

--- PupilTest2.py
+++ PupilTest2.py
@@ -15,7 +15,6 @@ import scipy.signal
 from nems_mod2 import *
 from newNEM import *
 import copy
-from import_test import *
 def avg_trials_test(data):
     avgs=np.nanmean(data,axis=1)
     return(avgs)
@@ -27,9 +26,9 @@ test2=FERReT(batch=294,cellid='eno048f-a1_nat_export.mat',n_coeffs=20,thresh=0.4
 
 #test2.raster_plot(stims=(0,1))
 
-FERReT.new_method=test_fn
+#FERReT.new_method=test_fn
 
-test2.new_method() #Adding an externally defined function
+#test2.new_method() #Adding an externally defined function
 #However, it is probably better to just define a subclass that inherits from FERReT
 #to add more functions
 
@@ -42,10 +41,11 @@ queue=('FIR','pupil_gain','pupil_gain')
 
 print(getattr(test2,'mse'))
 test2.run_fit(queue=queue,validation=0.5,reps=2)
-assem=test2.assemble(avgresp=False,useval=True,
-                                  save=False,filepath='/auto/users/shofer/code/nems/Saved_Data/test2.npy')
+test2.heatmap(model='FIR')
+#assem=test2.assemble(avgresp=False,useval=True,
+                                  #save=False,filepath='/auto/users/shofer/code/nems/Saved_Data/test2.npy')
 #assem=np.load('/auto/users/shofer/Saved_Data/test2.npy')[()]
-test2.plot_pred_resp(assem,stims='all',trials=(20,30))
+#test2.plot_pred_resp(assem,stims='all',trials=(20,30))
 
 
 #plt.figure(4,figsize=(15,5))
---
+++ testpack/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  8 16:48:06 2017
+
+@author: shofer
+"""
+##Initialization for testpack
