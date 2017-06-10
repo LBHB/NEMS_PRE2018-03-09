c8de4e6a2a1c0311c3c96aedbb857a53f3988eff
Noah Shofer <njshofer@gmail.com>
2017-06-09 18:12:17 -0700

    Update to modNEM, added functions to testpack

---
+++ testpack/DEXP.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  9 14:27:05 2017
+
+@author: shofer
+"""
+import numpy as np
+
+def create_DEXP(obj,**kwargs):
+    obj.fit_param['DEXP']=['DEXP']
+    obj.DEXP=np.ones([1,4]) 
+    obj.DEXP[0][1]=0
+    obj.DEXP[0][3]=0 
+    print('DEXP parameters created')
+
+
+def DEXP(obj,**kwargs):
+    ins=kwargs['data'] #data should be obj.current
+    v1=obj.DEXP[0,0]
+    v2=obj.DEXP[0,1]
+    v3=obj.DEXP[0,2]
+    v4=obj.DEXP[0,3]
+    output=v1-v2*np.exp(-np.exp(v3*(ins-v4)))
+    obj.current=output
+    return(output)
