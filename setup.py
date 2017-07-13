704dc76892913254bc589f8507f573cae5052c3f
Brad Buran <buran@ohsu.edu>
2017-07-13 09:31:30 -0700

    Add setup skeleton

---
+++ setup.py
@@ -0,0 +1,28 @@
+import codecs
+import os.path
+from setuptools import find_packages, setup
+
+NAME = 'NEMS'
+
+version = 'pre-alpha'
+
+with codecs.open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=NAME,
+    version=version,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=True,
+    author='LBHB',
+    author_email='lbhb.ohsu@gmail.com',
+    description='Neural encoding model system',
+    long_description=long_description,
+    url='http://neuralprediction.org',
+    install_requires=['numpy', 'scipy', 'matplotlib', 'flask', 'sqlalchemy'],
+    #setup_requires=['pytest-runner'],
+    #tests_require=['pytest'],
+    #license='MIT',
+    classifiers=[]
+)
