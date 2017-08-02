faa321666001d86f7ff543a620696b3bb2a71f9e
Jacob Pennington <jacob.pennington@wsu.edu>
2017-08-02 07:47:47 -0700

    changes to nems_fit_single

--- nems_fit_single.py
+++ nems_fit_single.py
@@ -3,11 +3,18 @@
 # This script runs nems_main.fit_single_model from the command line
 
 import nems.main as nems
-from nems.db import cluster_Session, tQueue
 import sys
 import os
-import nems.db as nd
-
+try:
+    import nems.db as nd
+    db_exists = True
+except Exception as e:
+    # If there's an error import nems.db, probably missing database
+    # dependencies. So keep going but don't do any database stuff.
+    print("Problem importing nems.db, can't update tQueue")
+    print(e)
+    db_exists = False
+        
 if __name__ == '__main__':
     
     # leftovers from some industry standard way of parsing inputs
@@ -20,17 +27,15 @@ if __name__ == '__main__':
     #action=parser.action[0]
     #updatecount=parser.updatecount[0]
     #offset=parser.offset[0]
-
     if 'QUEUEID' in os.environ:
-        queueid=os.environ['QUEUEID']
-        print("Starting QUEUEID={}".format(queueid))
-        conn=nd.cluster_engine.connect()
-        # tick off progress, job is live
-        sql="UPDATE tQueue SET complete=-1,progress=progress+1 WHERE id={}".format(queueid)
-        result = conn.execute(sql)
+        queueid = os.environ['QUEUEID']
     else:
-        queueid=0
+        queueid = 0
         
+    if queueid:
+        print("Starting QUEUEID={}".format(queueid))
+        nd.update_job_start(queueid)
+
     if len(sys.argv)<4:
         print('syntax: nems_fit_single cellid batch modelname')
         exit(-1)
@@ -45,35 +50,17 @@ if __name__ == '__main__':
     print("Done with fit.")
     
     # Edit: added code to save preview image. -Jacob 7/6/2017
-    path = stack.quick_plot_save(mode="png")
-    print("Preview saved to: {0}".format(path))
+    preview_file = stack.quick_plot_save(mode="png")
+    print("Preview saved to: {0}".format(preview_file))
     
     if queueid:
-        # mark job complete
-        # svd old-fashioned way of doing
-        #sql="UPDATE tQueue SET complete=1 WHERE id={}".format(queueid)
-        #result = conn.execute(sql)
-        #conn.close()
-   
-        cluster_session = cluster_Session()
-        # also filter based on note - should only be one result to match either
-        # filter, but double checks to make sure there's no conflict
-        note = "{0}/{1}/{2}".format(cellid, batch, modelname)
-                #.filter(tQueue.note == note)
-        qdata = (
-                cluster_session.query(tQueue)
-                .filter(tQueue.id == queueid)
-                .first()
-                )
-        if not qdata:
-            # Something went wrong - either no matching id, no matching note,
-            # or mismatch between id and note
-            print("Invalid query result when checking for queueid & note match")
-            print("/n for queueid: %s"%queueid)
+        nd.update_job_complete(queueid)
+        
+    if db_exists:
+        if queueid:
+            pass
         else:
-            qdata.complete = 1
-            cluster_session.commit()
-       
-        cluster_session.close()
-           
+            queueid = None
+        r_id = nd.save_results(stack, preview_file, queueid=queueid)
+        print("Fit results saved to NarfResults, id=%s"%r_id)
        
