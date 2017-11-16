9d9d6554a6765f1baceda880cf311d91d6da7c99
Ivar Thorson <thorsoni@ohsu.edu>
2017-11-16 11:34:53 -0800

    Jerb central repo works, but is not-bit-for-bit accurate; commenting it out of the API for now.

--- jerbstore/__main__.py
+++ jerbstore/__main__.py
@@ -5,14 +5,14 @@ from jerbstore.jerbstore_api import LocalJerbStore, CentralJerbStore
 app = Flask(__name__)
 api = Api(app)
 api.add_resource(LocalJerbStore,
-                 '/local/jid',
-                 '/local/jid/<string:jid>',
-                 resource_class_kwargs={'local_jerb_dir': '/home/ivar/jerbs/'})
-
-api.add_resource(CentralJerbStore,
                  '/jid',
                  '/jid/<string:jid>',
-                 resource_class_kwargs={'jerb_central_repo_dir':
-                                        '/home/ivar/central/'})
+                 resource_class_kwargs={'local_jerb_dir': '/home/ivar/jerbs/'})
+
+# api.add_resource(CentralJerbStore,
+#                  '/central/jid',
+#                  '/central/jid/<string:jid>',
+#                  resource_class_kwargs={'jerb_central_repo_dir':
+#                                         '/home/ivar/central/'})
 
 app.run(port=3000, host='127.0.0.1')
