e3368dde902826da8b2eb8eb5e408867497875d4
Stephen David <stephen.v.david@gmail.com>
2018-01-13 07:35:13 -0800

    adding some demo code support

---
+++ svd/simple_demo.py
@@ -0,0 +1,8 @@
+Use these public s3 files
+
+upload: ./gus021d-a2_NAT_pupil_fs100.mat to s3://nemspublic/sample_data/gus021d-a2_NAT_pupil_fs100.mat
+upload: ./gus021d-a2_NAT_resp_fs100.mat to s3://nemspublic/sample_data/gus021d-a2_NAT_resp_fs100.mat
+upload: ./gus021d-a2_NAT_stim_ozgf_c18_fs100.mat to s3://nemspublic/sample_data/gus021d-a2_NAT_stim_ozgf_c18_fs100.mat
+
+
+
