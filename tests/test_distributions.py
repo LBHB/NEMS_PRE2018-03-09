8073021c1face6a776c59cfb7a25fe8353d35b5c
Brad Buran <bburan@alum.mit.edu>
2018-01-16 15:08:39 -0800

    Update test_distributions to reflect bugfixes

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -1,21 +1,16 @@
-import pytest
 import numpy as np
-from scipy.stats import norm, beta
-from nems.distributions.normal import Normal
-from nems.distributions.half_normal import HalfNormal
-import matplotlib.pyplot as plt
+from nems.distributions.api import Normal, HalfNormal
+
+
+# TODO: How do we integrate plotting tests?
 
 
 def test_distributions():
-    
-    # Two normal distributions
-    d = Normal(mu=[-0.9, 0.2], sd=[1, 0.4])
-    d2 = HalfNormal(sd=[1, 0.4])
+    d1 = Normal(mu=[-0.9, 0.2], sd=[1, 0.4])
+    d2 = HalfNormal(sd=[[1, 0.4, .5], [0.3, 0.1, .7]])
 
-    # Test plotting (TODO: how to make this a useful test?)
-    # d.plot()
-    # d2.plot()
-
-    assert(100 == len(d.sample(100)))
-    assert(100 == len(d2.sample(100)))
-
+    n = 100
+    d1_sample = d1.sample(n)
+    d2_sample = d2.sample(n)
+    assert(d1_sample.shape == (n, 2))
+    assert(d2_sample.shape == (n, 2, 3))
