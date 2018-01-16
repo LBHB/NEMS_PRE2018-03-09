4eff1529c60426b5d1accec02dbf3f8de0c8401e
Ivar Thorson <thorsoni@ohsu.edu>
2018-01-15 16:11:40 -0800

    Moving some documentation in pure_function_mockup to README.md

--- pure_function_mockup.py
+++ pure_function_mockup.py
@@ -1,87 +1,70 @@
-# Stateless Module Demo
-
-# The more I think about it, the more I wonder if our real goal should just
-# be to exorcise all state from a module, turning modules into "pure functions"
-# (a.k.a. functions without side effects. Not that we shouldn't use classes,
-# but that we should keep focused on stateless functions when possible.
-#
-# Managing modules can be complicated precisely because they contain mutable
-# state. Given that state is usually easier when it is all in once place,
-# maybe packing the entire model into a single data structure isn't such a
-# crazy idea.
-#
-# The following shows a little demo of how that might look in general,
-# and for three cases that are not supported by the current version of NEMS:
-#
-#   1. "Per-module fitters", when each module uses a different sub-fitter
-#      and all modules are iterated through.
-#
-#   2. "Parameter fitting dropout", when some parameters are randomly NOT
-#      considered for optimization during the fitting process
-#
-#   3. "Module dropout", when the transformation of random modules are
-#      temporarily omitted during the fitting process
-#
-# The latter two techniques have been used extensively in deep learning
-# and allegedly make the fitting process more robust. And per-module
-# fitters could be a cleaner way of putting all the fitting code in a single
-# place rather than spreading it out and putting a little in each module,
-# which then requires that other fitters remove manually whenever they
-# do not want to do an "initial-fit".
-
-##############################################################################
-
-# ----------------------------------------------------------------------------
-# DATA LOADING AND SPLITTING
+# A demonstration of a minimalist model fitting system
 
 from nems.recording import Recording
 
-# Load the data: a dict of Signal objects
+# ----------------------------------------------------------------------------
+# DATA WRANGLING
+
+# Method #1: Load the data from a local directory
 rec = Recording.load('signals/gus027b13_p_PPS/')
 
-# Alternatively, from the (soon to be created) API:
-#URL = "neuralprediction.org:3003/signals?batch=273&cellid=gus027b13-a1"
-#sigs = fetch_signals_over_http(URL)
+# Method #2: Load the data using the (soon to be created, TODO) HTTP API:
+# URL = "neuralprediction.org:3003/signals?batch=273&cellid=gus027b13-a1"
+# rec = fetch_signals_over_http(URL)
 
-# For now, just assume that sigs looks roughly like this:
-# sigs = {'stim': signal.Signal(...),
-#         'resp': signal.Signal(...),
-#         'pupil': signal.Signal(...)}
 
-# Create est dataset from the first 80%, and val from the last 20%
-# (The Signal class has other ways of splitting/combining files & signals)
+# ----------------------------------------------------------------------------
+
+# Create estimation dataset from the first 80% of the recording, and the
+# validation data set from the last 20%. (Note: the Recording class has other
+# ways of splitting/combining things)
 est, val = rec.split_at_time(0.8)
 
-# Open question: even though it is only a few lines, how and where should
+# TODO: Open question: even though it is only a few lines, how and where should
 # this information be recorded? The data set that a model was trained on
 # is relevant information that should be serialized and recorded somewhere.
 # save_to_disk('/some/path/model1_dataspec.json',
 #              json.dumps({'URL': URL, 'est_val_split_frac': 0.8}))
 
+
 # ----------------------------------------------------------------------------
-# MODELSPEC
+# CREATING A MODEL FROM A MODELSPEC
+
 # A 'modelspec' object is a simple datastructure that defines the entire
 # model, and is easily saved/loaded to disk. It is essentially a serialization
-# of the current "stack" object. You can define it three ways:
+# of a Model object. You can define it three ways:
 
-# Modelspec Creation Method #1: specify it manually
-modelspec = [{'fn': 'nems.modules.fir',       # The pure function to call
-              'phi': [1.0, 0.9, 20.8, ...],   # current params during fitting
-              'prior': [0.0, 0.0, 0.1, ...],  # a.k.a. Initialization params
-              'posterior': None,              # a.k.a. Params after fitting
-              'plotfn': 'nems.plots.plot_strf'}, # A plotting function
-             {'fn': 'nems.modules.nl/dexp',
-              'phi': [0.54, 1.982, 9.1],
-              'prior': [1, 2, 5],
-              'posterior': None,
-              'plotfn': 'nems.plots.plot_nl'},
-             ...]
-
-# Modelspec Creation Method #2: load a from disk
-# modelspec = json.loads('/path/to/model1.json')
-
-# Modelspec Creation Method #3: create from "shorthand/default" keyword string
+# Method #1: create from "shorthand/default" keyword string
 # modelspec = keywords_to_modelspec('fir30_dexp')
+
+# Method #2: load a JSON from disk
+# modelspec = json.loads('modelspecs/model1.json')
+
+# Method #3: specify it manually
+modelspec = [{'fn': 'nems.modules.weight_channels',
+              'phi' : TODO}
+             {'fn': 'nems.modules.fir',       # The pure function to call
+              'phi': {                        # The parameters that may change
+                  'mu': {
+                    # expected distribution
+                    'prior': ('Normal', {'mu': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                                         'sd': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}),
+                    # fitted distribution (if applicable)
+                    'posterior': None,
+                    # initial scalar value (typically the mean of the prior)
+                    'initial': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                    # fitted scalar value (if applicable)
+                    'final': None,
+                    },
+                  'sd': {
+                    'prior': ('HalfNormal', {'sd': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}),
+                    'posterior': None,
+                    'initial': 1,
+                    'final': None,
+                    }
+                  },
+              'plotfn': 'nems.plots.plot_strf'}, # A plotting function
+             ]
 
 # ----------------------------------------------------------------------------
 # FITTING ALGORITHMS
@@ -90,14 +73,14 @@ modelspec = [{'fn': 'nems.modules.fir',       # The pure function to call
 #
 # | Function Signature                   | NAME. Description                     |
 # |--------------------------------------+---------------------------------------|
-# | f(modelspec, data) -> pred           | EVALUATOR. Makes a model's prediction |
+# | f(mspec, data) -> pred               | EVALUATOR. Makes a model's prediction |
 # | f(pred) -> error                     | METRIC. Evaluates predictive accuracy |
-# | f(modelspec, metric, evaluator) -> modelspec | FITTER. Finds a better modelspec      |
+# | f(mspec, metric, evaluator) -> mspec | FITTER. Finds a better modelspec      |
 #
 # where:
 #   data       is a dict of signals, like {'stim': ..., 'resp': ..., ...}
 #   pred       is a dict of signals, just like 'data' but containing 'pred'
-#   modelspec  is defined as above
+#   mspec      is the modelspec data type, as was defined above
 
 def evaluator(data_in, modelspec):
     ''' This should probably be a put into a StackModel class; but
@@ -113,6 +96,7 @@ metric = lambda data: nems.metrics.MSE(data['resp'], data['pred'])
 # the estimation data set, and never the validation one)
 from functools import partial
 est_evaluator = partial(evaluator, est)
+
 # TODO: If desired, evaluator could include inner CV loop wrapper as well
 
 # Perform the fit on est data set using standard gradient_descent
@@ -127,15 +111,13 @@ for m in nems.metrics.get_some_list_of_metric_functions:
 save_to_disk('/some/path/model1.json', json.dumps(modelspec_fitted))
 save_to_disk('/some/path/model1_results.json', json.dumps(results))
 
-
 # Plot the prediction vs reality
 # phi_distributions.plot('/some/other/path.png')
 
-# Plot confidence intervals
-# phi_EV = 
+# Plot prediction and confidence intervals
 # pred_EV = model.evaluate(phi_distributions.expected_value(), val)
-# pred_10 = model.evaluate(phi_distributions.percentile(10), val)
-# pred_90 = model.evaluate( phi_distributions.percentile(90), val)
+# pred_10 = model.evaluate(phi_distributions.ppf(10), val)
+# pred_90 = model.evaluate( phi_distributions.ppf(90), val)
 # plot_signals('/some/path.png', pred_EV, pred_10, pred_90, ...)
 
 # ----------------------------------------------------------------------------
@@ -166,7 +148,7 @@ save_to_disk('/some/path/model1_results.json', json.dumps(results))
 #    modelspec gives a distribution rather than a single parameter);
 #
 # 2) We may want to develop different "packers" and "unpackers" that strive to
-#    keep the parameter search space (the "stepspace) linear, even if the
+#    keep the parameter search space (the "stepspace") linear, even if the
 #    parameters themselves create a fairly nonlinear space.
 #
 # 3) By not assuming anything about the stepspace, we can easily change its
@@ -221,7 +203,7 @@ def random_subset_fitter(modelspec, cost_fn,
     while not stop_cond(stepinfo):
         (packer, stepper, unpacker) = make_random_subset_triplet()  # TODO
         stepspace_point = packer(modelspec)
-        best_spec = try_every_step(stepspace_point, unpacker, cost_fn)
+        best_spec = stepper(stepspace_point, unpacker, cost_fn)
         spec = unpacker(best_spec)
     return spec
 
