430156c3eed4a997b7c9034fc00454e44a097cee
hellerc <hellerc@ohsu.edu>
2018-01-29 11:22:26 -0800

    utils to make raster from spike times

--- charlie_population_coding/charlie_random_utils.py
+++ charlie_population_coding/charlie_random_utils.py
@@ -8,6 +8,92 @@ Created on Thu Jan 18 17:28:43 2018
 import numpy as np
 
 
+
+
+def create_raster(spike_dict, fs, tend):
+    '''
+    Given spike times, sampling rate, and length of recording, create a spike 
+    raster, (time x neurons)
+    
+    CRH, 01-27-2018
+    '''    
+    cellids=[x for x in spike_dict.keys()]
+    ncells=len(cellids)
+    exptbins=int(tend*fs)
+    r=np.zeros((exptbins,ncells))
+    
+    for cell, cid in enumerate(cellids):
+        spike_times=spike_dict[cid]
+        exptbins=int(tend*fs)
+        spikebins=np.array([int(x) for x in fs*spike_times])
+        spikebins=spikebins[spikebins<exptbins]
+        for i in spikebins:
+            r[i, cell]+=1
+
+    return r
+    
+    
+def create_event_raster(spike_dict, event_times, fs, stimulus=None):
+    '''
+    Given spike times, event times, sampling rate, length of recording
+    produce a raster sorted by trial stimulus and repetition. If stimulus is 
+    specified, return a raster of tbins x stim x rep x neuron
+    '''
+    if stimulus is None:
+        stimuli=len(event_times['epoch_name'].unique())-3
+        trials=int(event_times['epoch_name'].value_counts().iloc[3])  # get first that isn't trial, pre, or post stim
+        bins=int(round(np.max(event_times['StopTime']-event_times['StartTime']),2)*fs)
+        ncells=len([x for x in spike_dict.keys()])
+        
+        r = np.zeros((bins,stimuli,trials,ncells))
+        
+        stims=event_times['epoch_name'].unique()
+        stims = [e for e in stims if e not in {'PreStimSilence', 'TRIAL','PostStimSilence'}]
+        
+        cellids=np.sort(np.array([x for x in spike_dict.keys()]))
+        
+        for c, cid in enumerate(cellids):
+            print('loading raster for: ' + cid)
+            for s, stim in enumerate(stims):
+                reps = sum(event_times['epoch_name']==stim)
+                events_this_stim=event_times[event_times['epoch_name']==stim]
+                for rep in range(0,reps):
+                    start=int(events_this_stim['StartTime'].loc[rep]*fs)
+                    end=int(events_this_stim['StopTime'].loc[rep]*fs)
+                    spikes=[int(x) for x in (spike_dict[cid]*fs) if x > start and x < end]
+                    spikes=np.array(spikes)-int(events_this_stim['StartTime'].loc[rep]*fs)
+                    for t in spikes:
+                        r[t,s,rep,c]+=1
+        return r
+    else:
+        stimuli=1
+        trials=int(event_times[event_times['epoch_name']==stimulus]['epoch_name'].value_counts().iloc[0])  # get first that isn't trial, pre, or post stim
+        bins=int(round(np.max(event_times['StopTime']-event_times['StartTime']),2)*fs)
+        ncells=len([x for x in spike_dict.keys()])
+        
+        r = np.zeros((bins,stimuli,trials,ncells))
+        
+        stims=[stimulus]
+        
+        cellids=np.sort(np.array([x for x in spike_dict.keys()]))
+        
+        for c, cid in enumerate(cellids):
+            print('loading raster for: ' + cid)
+            for s, stim in enumerate(stims):
+                reps = sum(event_times['epoch_name']==stim)
+                events_this_stim=event_times[event_times['epoch_name']==stim]
+                for rep in range(0,reps):
+                    start=int(events_this_stim['StartTime'].loc[rep]*fs)
+                    end=int(events_this_stim['StopTime'].loc[rep]*fs)
+                    spikes=[int(x) for x in (spike_dict[cid]*fs) if x > start and x < end]
+                    spikes=np.array(spikes)-int(events_this_stim['StartTime'].loc[rep]*fs)
+                    for t in spikes:
+                        r[t,s,rep,c]+=1
+                        
+        
+        return r
+
+
 def remove_nans(runclass, options, r=None, p=None):
     '''
     Specific to this script. Remove nans based on given runclass. Return r and p
