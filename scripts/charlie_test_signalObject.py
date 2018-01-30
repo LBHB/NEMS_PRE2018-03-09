94550c160008f73008853f2301f34f63c486e147
hellerc <hellerc@ohsu.edu>
2018-01-29 16:07:15 -0800

    comment out nems_config imports to get baphy to work in dev

--- scripts/charlie_test_signalObject.py
+++ scripts/charlie_test_signalObject.py
@@ -5,7 +5,12 @@ Created on Mon Jan 29 11:26:10 2018
 
 @author: hellerc
 """
-
+import sys
+sys.path.append('/auto/users/hellerc/NEMS/')
+import nems.utilities.baphy
 
 # load data in first (matrix)
 
+parmfilepath='/auto/data/daq/Tartufo/TAR010/TAR010c16_p_NAT.m'
+options={'rasterfs': 100, 'includeprestim': True, 'stimfmt': 'ozgf', 'chancount': 18, 'cellid': 'all', 'pupil': True}
+event_times, spike_dict, stim_dict, state_dict = nems.utilities.baphy.baphy_load_recording(parmfilepath,options)
