3527f1a8c8f3ec7a30d24dd3e8e1a8d785b69655
hellerc <hellerc@ohsu.edu>
2018-01-29 17:59:52 -0800

    added script to illustrate loading of parmfile into a signal object

--- scripts/charlie_test_signalObject.py
+++ scripts/charlie_test_signalObject.py
@@ -6,13 +6,48 @@ Created on Mon Jan 29 11:26:10 2018
 @author: hellerc
 """
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
 sys.path.append('/auto/users/hellerc/NEMS/')
-import nems.utilities.baphy
+from nems.utilities.baphy import baphy_load_recording, spike_time_to_raster
+from nems.signal import Signal
+ 
+# load data in first (create a spike/response matrix)
+parmfilepath = '/auto/data/daq/Tartufo/TAR010/TAR010c16_p_NAT.m'
+options = {'rasterfs': 100, 'includeprestim': True, 'stimfmt': 'ozgf', 'chancount': 18, 'cellid': 'all', 'pupil': True}
 
-# load data in first (matrix)
+# load baphy parmfile
+out = baphy_load_recording(parmfilepath,
+                           options)
+# unpack output
+event_times, spike_dict, stim_dict, state_dict = out
+event_times['start_index']=[int(x) for x in event_times['StartTime']*100]
+event_times['end_index']=[int(x) for x in event_times['StopTime']*100]
 
-parmfilepath='/auto/data/daq/Tartufo/TAR010/TAR010c16_p_NAT.m'
-options={'rasterfs': 100, 'includeprestim': True, 'stimfmt': 'ozgf', 'chancount': 18, 'cellid': 'all', 'pupil': True}
-event_times, spike_dict, stim_dict, state_dict = nems.utilities.baphy.baphy_load_recording(parmfilepath,options)
+# r is response matrix, created from dictionary of spike times
+r = spike_time_to_raster(spike_dict=spike_dict,
+                         fs=100,
+                         event_times=event_times)
 
-r = nems.utilities.baphy.spike_time_to_raster(spike_dict=spike_dict,fs=100,event_times=event_times)
+# Create the signal object spike raster (r)
+sig = Signal(fs=100,
+             matrix=r[0],
+             name='test_name',
+             recording='test_recording',
+             epochs=event_times)
+
+# Fold matrix over all stimuli, returning a dictionary where keys are stimuli 
+# each value in the dictionary is (reps X cell X bins)
+folded_matrix = sig.multi_fold_by(stim_dict.keys())
+
+# Average over all reps of each stim and save into dict called psth. Same 
+# formate as folded_matrix
+psth = dict()
+for k in folded_matrix.keys():
+    psth[k] = np.mean(folded_matrix[k],
+                      axis=0)
+    
+# Invert the folding to unwrap the psth back out into a predicted spike_dict by 
+# simply replacing all epochs in the signal with their psth
+psth_sig = sig.replace_epochs(psth)
