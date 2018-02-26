6219ff71d9ef4a71dd1a3b2568f03c3b9317c703
Ivar Thorson <thorsoni@ohsu.edu>
2018-02-26 10:26:29 -0800

    Refactoring of file.py to correct minor docs and a potential logic bug.

--- nems/plots/file.py
+++ nems/plots/file.py
@@ -1,18 +1,17 @@
 import os
 import logging
-logging.basicConfig(level=logging.INFO)
-
 import matplotlib.image as mpimg
-
 import nems.modelspec as ms
+
 
 def save_figure(fig, filepath=None, modelspecs=None, save_dir='/tmp',
                 format='png'):
-    """Saves the given matplotlib figure object, using either a specific
+    '''
+    Saves the given matplotlib figure object, using either a specific
     filepath or a path determined from a list of modelspecs and a
     directory.
 
-    Argumets
+    Arguments
     --------
     fig : matplotlib figure
         Any matplotlib figure object
@@ -29,6 +28,7 @@ def save_figure(fig, filepath=None, modelspecs=None, save_dir='/tmp',
     save_dir : str
         Specifies the base directory in which to save the figure,
         if modelspecs are used.
+
     format : str
         Specifies the file format that should be used to save the figure.
         Compatible choices depend on which matplotlib backend is running,
@@ -39,24 +39,26 @@ def save_figure(fig, filepath=None, modelspecs=None, save_dir='/tmp',
     fname : str
         The filepath that was ultimately used for storage.
 
-    """
-    if filepath:
+    '''
+    if filepath and not modelspecs:
         fname = filepath
+    elif modelspecs and not filepath:
+        fname = _get_figure_filepath(save_dir, modelspecs, format)
     else:
-        if not modelspecs:
-            raise ValueError("save_figure() must be provided either"
-                             "a filepath or a list of modelspecs.")
-        fname = _get_figure_name(save_dir, modelspecs, format)
+        raise ValueError("save_figure() must be provided either"
+                         "a filepath or a list of modelspecs.")
     logging.info("Saving figure as: {}".format(fname))
     fig.savefig(fname)
     return fname
 
+
 def load_figure_img(filepath=None, modelspecs=None, load_dir=None,
                     format='png'):
-    """Loads a saved figure image as a numpy array that can be displayed
+    '''
+    Loads a saved figure image as a numpy array that can be displayed
     inside python using matplotlib.pyplot.imshow().
 
-    Argumets
+    Arguments
     --------
     filepath : str or fileobj
         Specifies the location where the image was stored.
@@ -79,24 +81,26 @@ def load_figure_img(filepath=None, modelspecs=None, load_dir=None,
     img : numpy ndarray
         Array containing the image data.
 
-    """
-    if filepath:
+    '''
+    if filepath and not modelspecs:
         fname = filepath
+    elif modelspecs and not filepath:
+        fname = _get_figure_filepath(load_dir, modelspecs, format)
     else:
-        if not modelspecs:
-            raise ValueError("load_figure_img() must be provided either"
-                             "a filepath or a list of modelspecs.")
-        fname = _get_figure_name(load_dir, modelspecs, format)
+        raise ValueError("load_figure_img() must be provided either"
+                         "a filepath or a list of modelspecs.")
     logging.info("Loading figure image from: {}".format(fname))
     img = mpimg.imread(fname)
     return img
 
+
 def load_figure_bytes(filepath=None, modelspecs=None, load_dir=None,
                       format='png'):
-    """Loads a saved figure image as a bytes object that can be used
+    '''
+    Loads a saved figure image as a bytes object that can be used
     by the web UI or other functions.
 
-    Argumets
+    Arguments
     --------
     filepath : str or fileobj
         Specifies the location where the image was stored.
@@ -119,22 +123,25 @@ def load_figure_bytes(filepath=None, modelspecs=None, load_dir=None,
     img : bytes object
         Contains the raw data for the loaded image.
 
-    """
-    if filepath:
+    '''
+    if filepath and not modelspecs:
         fname = filepath
+    elif modelspecs and not filepath:
+        fname = _get_figure_filepath(load_dir, modelspecs, format)
     else:
-        if not modelspecs:
-            raise ValueError("load_figure_img() must be provided either"
-                             "a filepath or a list of modelspecs.")
-        fname = _get_figure_name(load_dir, modelspecs, format)
+        raise ValueError("load_figure_img() must be provided either"
+                         "a filepath or a list of modelspecs.")
     logging.info("Loading figure image from: {}".format(fname))
     with open(fname, 'rb') as f:
         img = f.read()
     return img
 
-def _get_figure_name(directory, modelspecs, format):
-    """Determins a filepath based on a directory, a list of modelspecs,
-    and a file format."""
+
+def _get_figure_filepath(directory, modelspecs, format):
+    '''
+    Returns a filepath based on a directory, a list of modelspecs,
+    and a file format.
+    '''
     # TODO: Probably need a smarter way to do figure names since figures
     #       can come from an arbitrary number of modelspecs. Could just
     #       concatenate names for each modelspec? But that would mean some
