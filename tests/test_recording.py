b185eb2a69212954b2f6046f4f465d327d223640
Ivar Thorson <thorsoni@ohsu.edu>
2018-03-06 10:48:28 -0800

    Adding an afterthought to test_recording: s3 credential management mean that we should probably just do HTTPS for most clients.

--- tests/test_recording.py
+++ tests/test_recording.py
@@ -7,16 +7,17 @@ def test_recording_loading():
     Test the loading and saving of files to various HTTP/S3/File routes.
     '''
     # Local filesystem
-    #rec0 = Recording.load("/home/ivar/git/nems/signals/TAR010c-18-1.tar.gz")
-    #rec1 = Recording.load("/auto/data/tmp/recordings/TAR010c-18-1.tar.gz")
-    #rec2 = Recording.load("file:///auto/data/tmp/recordings/TAR010c-18-1.tar.gz")
+    rec0 = Recording.load("/home/ivar/git/nems/signals/TAR010c-18-1.tar.gz")
+    rec1 = Recording.load("/auto/data/tmp/recordings/TAR010c-18-1.tar.gz")
+    rec2 = Recording.load("file:///auto/data/tmp/recordings/TAR010c-18-1.tar.gz")
 
     # HTTP
-    #rec3 = Recording.load("http://potoroo:3001/recordings/TAR010c-18-1.tar.gz")
-    #rec4 = Recording.load("http://potoroo/baphy/271/TAR010c-18-1")
+    rec3 = Recording.load("http://potoroo:3001/recordings/TAR010c-18-1.tar.gz")
+    rec4 = Recording.load("http://potoroo/baphy/271/TAR010c-18-1")
 
     # S3
     # Direct access (would need AWS CLI lib? Maybe not best idea!)
+    # TODO: Requires s3 credentials in environment. Probably best if on server only; put in nems_db?
     # rec5 = Recording.load('s3://mybucket/myfile.tar.gz')
     # Indirect access via http:
     rec6 = Recording.load('https://s3-us-west-2.amazonaws.com/nemspublic/sample_data/TAR010c-18-1.tar.gz')
