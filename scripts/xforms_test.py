8b916b92d5eff137398f250136652e59a0ee79af
Ivar Thorson <thorsoni@ohsu.edu>
2018-03-06 18:03:05 -0800

    First step with xforms seems to work. It saves a logfile and loads a remote recording.

--- scripts/xforms_test.py
+++ scripts/xforms_test.py
@@ -1,3 +1,15 @@
 # Ivar tries out the xforms concept, similar to how NARF used to work
 
+import nems.xforms as xforms
+
 recordings = ['http://potoroo/recordings/TAR010c-02-1.tar.gz']
+
+
+xfspec = [{'xf': 'nems.xforms.load_recordings',
+           'xf_kwargs': {'recording_uri_list': recordings}}]
+#          {'xf': ,
+#           'xf_kwargs': {}}]
+
+ctx = xforms.evaluate(xfspec)
+
+print(ctx)
